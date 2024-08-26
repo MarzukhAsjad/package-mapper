ccf7a18d303f671dcd0a54ae54eed6a307b6c7dd
MarzukhAsjad <marz.asjad00@gmail.com>
2024-08-26 17:43:41 +0800

    feat: Implement CodeCrawler interface and include classesMap parameter in visualize method

--- src/main/java/com/example/mizookie/packagemapper/services/CodeCrawler.java
+++ src/main/java/com/example/mizookie/packagemapper/services/CodeCrawler.java
@@ -1,7 +1,8 @@
 package com.example.mizookie.packagemapper.services;
+import java.util.Map;
 
 public interface CodeCrawler {
     String crawl(String path); // path to the files which return the code to parse
     void parse(String data); // data from the file to parse
-    void visualize(); // visualize the parsed data
+    void visualize(Map<String, String> classesMap); // visualize the parsed data
 }
