3f63416e3dfe431a57bc7820549141de2296696f
MarzukhAsjad <marz.asjad00@gmail.com>
2024-08-26 11:34:27 +0800

    feat: Add GithubRepositoryInfoRequest DTO for handling repository information

---
+++ src/main/java/com/example/mizookie/packagemapper/dto/user/GithubRepositoryInfoRequest.java
@@ -0,0 +1,17 @@
+package com.example.mizookie.packagemapper.dto.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GithubRepositoryInfoRequest {
+
+    @JsonProperty("url")
+    String repositoryUrl;
+}
