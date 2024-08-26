61f9043918160e053c605cbf00935c7a651127d0
MarzukhAsjad <marz.asjad00@gmail.com>
2024-08-26 11:29:35 +0800

    chore: Add initial project files and configurations

---
+++ src/main/java/com/example/mizookie/packagemapper/PackagemapperApplication.java
@@ -0,0 +1,13 @@
+package com.example.mizookie.packagemapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackagemapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackagemapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mizookie/packagemapper/PackagemapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mizookie.packagemapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackagemapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
