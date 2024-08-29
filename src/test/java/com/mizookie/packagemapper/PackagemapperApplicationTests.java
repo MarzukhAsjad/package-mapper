38cb0c2f29df5ab22e3514f99ea7c19470fdc1e2
MarzukhAsjad <marz.asjad00@gmail.com>
2024-08-29 12:44:06 +0800

    chore: Update .gitignore to ignore logs and environment files

---
+++ src/test/java/com/mizookie/packagemapper/PackagemapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mizookie.packagemapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackagemapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
