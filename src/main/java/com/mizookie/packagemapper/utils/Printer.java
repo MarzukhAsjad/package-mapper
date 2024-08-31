d245cf02316904380ff662f204ade48e8819b35a
MarzukhAsjad <marz.asjad00@gmail.com>
2024-08-31 23:01:31 +0800

    Printer now creates analysis log based on repository name and stores logs there

--- src/main/java/com/mizookie/packagemapper/utils/Printer.java
+++ src/main/java/com/mizookie/packagemapper/utils/Printer.java
@@ -2,31 +2,56 @@ package com.mizookie.packagemapper.utils;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 
 import lombok.extern.slf4j.Slf4j;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 public class Printer {
     // private constructor to hide the implicit public one
     private Printer() { }
 
+    // Analysis file instance for logging
+    private Path analysisFile;
+
+    @Value("${logs.directory}")
+    private String logsDirectory;
+
+    @Value("${analysis.directory}")
+    private String analysisDirectory;
+
     // Print to console
-    public static void log(String message) {
+    public void log(String message) {
         log.info(message);
         writeToFile(message);
     }
 
-    // Write to file
-    private static void writeToFile(String message) {
-        String currentDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    // Create analysis log
+    public void createAnalysisLog(String repoString) {
+        log.info("Creating analysis log for repository: " + FileService.getFileNameOnly(repoString));
+        String analysisLog = "analysis-" + FileService.getFileNameOnly(repoString) + ".log";
+        analysisFile = Paths.get(analysisDirectory, analysisLog);
         try {
-            String fileName = "LOGS-" + currentDate + ".log";
-            FileWriter writer = new FileWriter(fileName, true);
+            Files.createDirectories(analysisFile.getParent());
+            Files.createFile(analysisFile);
+        } catch (IOException e) {
+            log.error("An error occurred while creating the analysis log file.");
+            e.printStackTrace();
+        }
+    }
+
+    // Write to file
+    private void writeToFile(String message) {
+        try {
+            FileWriter writer = new FileWriter(analysisFile.toFile(), true);
             writer.write(message + "\n");
             writer.close();
-            log.info("Successfully logged to the file.");
         } catch (IOException e) {
             log.error("An error occurred while writing to the file.");
             e.printStackTrace();
